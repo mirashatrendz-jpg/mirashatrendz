8dfc2a026efbe09d4df70b3a517351774022b8a8
Sirin JH <mirashatrendz@gmail.com>
2026-05-19 00:27:00 +0530

    name change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const products = [
     {
         id: 1,
-        name: "Royal Eternity Necklace 123",
+        name: "Royal Eternity Necklace",
         price: 150,
         category: "Necklaces",
         image: "Ring-1.jpg"
