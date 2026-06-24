5a27a65cbd27031c798d17b43b5f9d8512c3f9ab
mirashatrendz-jpg <mirashatrendz@gmail.com>
2026-06-24 07:14:49 +0400

    KK

--- app.js
+++ app.js
@@ -484,6 +484,13 @@ const products = [
         category: "AD Necklaces",
         image: "RubyBlossomNecklaceSet.jpeg"
     },
+       {
+        id: 3,
+        name: "Snow Flakes Diamond Necklace",
+        price: 49,
+        category: "AD Necklaces",
+        image: "SnowFlakesDiamondNecklace.jpeg"
+    },
      {
         id: 3,
         name: "Quilted-Texture Adjustable Ring",
