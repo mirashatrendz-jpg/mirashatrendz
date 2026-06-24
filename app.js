49e1297da62b59d8cc6f95cb8ddb8409bcb832fd
mirashatrendz-jpg <mirashatrendz@gmail.com>
2026-06-24 06:46:45 +0400

    OKJN

--- app.js
+++ app.js
@@ -402,6 +402,13 @@ const products = [
     },
      {
         id: 1,
+        name: "Minimal Diamond Vine Choker",
+        price: 39,
+        category: "Necklaces",
+        image: "MinimalDiamondVineChoker.jpeg"
+    },
+     {
+        id: 1,
         name: "Shanku Chakra Namam Necklace Set",
         price: 39,
         category: "Necklaces",
