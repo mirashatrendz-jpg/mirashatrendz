b5f8725da1baf2720abc4a235a893b3fa3443f8b
mirashatrendz-jpg <mirashatrendz@gmail.com>
2026-06-26 02:45:46 +0400

    poj

--- app.js
+++ app.js
@@ -813,6 +813,13 @@ const products = [
     },
      {
         id: 6,
+        name: "Tara Stud",
+        price: 9,
+        category: "Anti-Tarnish",
+        image: "TaraStud.jpeg"
+    },
+     {
+        id: 6,
         name: "Alba Stud",
         price: 9,
         category: "Anti-Tarnish",
