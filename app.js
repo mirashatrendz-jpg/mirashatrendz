6aa4ba727eb11210195a718e5239c99d64bf0173
mirashatrendz-jpg <mirashatrendz@gmail.com>
2026-07-26 03:29:41 +0400

    jhbvn

--- app.js
+++ app.js
@@ -402,7 +402,7 @@ const products = [
     },
      {
         id: 58,
-        name: "Minimal Diamond Vine Choker",
+        name: "Minimal Diamond Choker",
         price: 39,
         category: "AD Necklace",
         image: "MinimalDiamondVineChoker.webp",
